9ce0d8bb202d24b506f95e80130bde4abe46ebb0
vanessa082 <wahvanessa22@gmail.com>
2023-12-12 18:31:42 +0100

    correcting linter

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 const nameField = document.querySelector('.username')
 const ageField = document.querySelector('.userage')
-const searchBtn = document.querySelector('.btn')
-const listofstudent = document.querySelector(".listofstudent")
+const listofstudent = document.querySelector('.listofstudent')
 const form = document.getElementById('userform')
 
-let arr_users = [
+let arrUsers = [
   { name: 'Wah Vanessa Tata', age: 20 },
   { name: 'Fai Ferry', age: 30 },
   { name: 'Fonyuy Carl', age: 28 },
-  { name: 'Akwi Abigail', age: 22 },
+  { name: 'Akwi Abigail', age: 22 }
 ]
 
 const getInitials = (str) => {
   let initial = str[0]
   for (let i = 0; i < str.length; i++) {
-    if (str[i] === ' ')
-      initial += "." + str[i + 1]
+    if (str[i] === ' ') {
+      initial += '.' + str[i + 1]
+    }
   }
   return initial.toUpperCase()
 }
 
 const createTemplate = (user) => {
   //
-  const btnId = user.name.split(" ").join("-")
+  const btnId = user.name.split(' ').join('-')
   return `
     <tr>
         <td>${getInitials(user.name)}</td>
-        <td class="nam">${user.name}</td>
-        <td class="ag">${user.age}</td>
-        <td><button class="delete-btn" id=${btnId}>X</button></td>
+        <td class='nam'>${user.name}</td>
+        <td class='ag'>${user.age}</td>
+        <td><button class='delete-btn' id=${btnId}>X</button></td>
     </tr>`
 }
 
@@ -53,9 +53,9 @@ const displayUsers = (users_arr) => {
 // SEARCHING
 
 const filterStudents = (searchname, searchage) => {
-  const results = arr_users.filter((user) => {
-    const lowerSearchName = searchname.toLowerCase();
-    const lowerUserName = user.name.toLowerCase(); // creating a lowercase version of user.name
+  const results = arrUsers.filter((user) => {
+    const lowerSearchName = searchname.toLowerCase()
+    const lowerUserName = user.name.toLowerCase() // creating a lowercase version of user.name
 
     if ((lowerSearchName && lowerUserName.includes(lowerSearchName)) || (searchage && user.age === searchage)) {
       return true
@@ -65,7 +65,7 @@ const filterStudents = (searchname, searchage) => {
   return results
 }
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault()
   const name = nameField.value
   const age = parseInt(ageField.value)
@@ -80,27 +80,28 @@ form.addEventListener("submit", (e) => {
 // DELETING USERS
 
 const setupEVentListeners = () => {
-  const deleteButtons = document.querySelectorAll(".delete-btn")
+  const deleteButtons = document.querySelectorAll('.delete-btn')
 
   deleteButtons.forEach((btn) => {
-    btn.addEventListener("click", (event) => {
+    btn.addEventListener('click', (event) => {
       const userName = event.target.id
 
-      const actualName = userName.split("-").join(" ")
+      const actualName = userName.split('-').join(' ')
 
-      const update = arr_users.filter((user) => {
+      const update = arrUsers.filter((user) => {
         if (user.name !== actualName) {
           return true
         }
+        return false
       })
 
       console.log(userName, actualName)
 
-      arr_users = update
+      arrUsers = update
 
       displayUsers(update)
     })
   })
 }
 
-displayUsers(arr_users)
+displayUsers(arrUsers)
