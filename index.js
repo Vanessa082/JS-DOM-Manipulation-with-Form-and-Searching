fbfa0bb2d4809de57d35b74595c8617b06dec2a2
vanessa082 <wahvanessa22@gmail.com>
2023-12-12 23:30:42 +0100

    correcting linter

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const nameField = document.querySelector('.username')
 const ageField = document.querySelector('.userage')
 const listofstudent = document.querySelector('.listofstudent')
 const form = document.getElementById('userform')
+const reset = document.querySelector('.material-symbols-outlined')
 
 let arrUsers = [
   { name: 'Wah Vanessa Tata', age: 20 },
   { name: 'Fai Ferry', age: 30 },
   { name: 'Fonyuy Carl', age: 28 },
-  { name: 'Akwi Abigail', age: 22 }
+  { name: 'Akwi Abigail', age: 22 },
+  { name: 'Joshua B', age: 26 },
+  { name: 'Emmanuel Man', age: 30 },
+  { name: 'Fonyuy Carlson', age: 28 },
+  { name: 'Ewi Josepha', age: 26 },
+  { name: 'Wah Vanessa', age: 21 },
+  { name: 'Fai Rash Shey', age: 30 },
+  { name: 'Faith Berinyuy', age: 28 },
+  { name: 'Phiip Abigail Penn', age: 22 }
 ]
 
 const getInitials = (str) => {
@@ -20,11 +29,11 @@ const getInitials = (str) => {
   return initial.toUpperCase()
 }
 
-const createTemplate = (user) => {
+const createTemplate = (user, i) => {
   //
-  const btnId = user.name.split(' ').join('-')
+  const btnId = user.name.split(' ').join('-') // condition ? run true verson : run false version of code
   return `
-    <tr>
+    <tr class="${i % 2 === 0 ? "" : "gray-tr"}">
         <td>${getInitials(user.name)}</td>
         <td class='nam'>${user.name}</td>
         <td class='ag'>${user.age}</td>
@@ -39,15 +48,23 @@ const displayUsers = (usersAarr) => {
             <th>Initials</th>
             <th>Name</th>
             <th>Age</th>
-            <th>X</th>
+            <th>Delete</th>
         </tr>
     `
 
-  usersAarr.forEach((user) => {
-    listofstudent.innerHTML += createTemplate(user)
-  })
+  if (usersAarr.length > 0) {
+    usersAarr.forEach((user, i) => {
+      listofstudent.innerHTML += createTemplate(user, i)
+    })
 
-  setupEVentListeners()
+    setupEVentListeners()
+  } else {
+    listofstudent.innerHTML += `
+      <tr>
+          <td colspan="4" class="result">No such Student found </td>
+      </tr>
+    `
+  }
 }
 
 // SEARCHING
@@ -106,3 +123,10 @@ const setupEVentListeners = () => {
 }
 
 displayUsers(arrUsers)
+
+// RESET USERS
+
+reset.addEventListener('click', () => {
+  // window.location.reload()
+  displayUsers(arrUsers) // best method
+})
